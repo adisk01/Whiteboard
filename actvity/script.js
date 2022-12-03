eb687520a9748137ec1b91b18d8b16f0303d40ca
adisk01 <81865578+adisk01@users.noreply.github.com>
2022-12-03 13:16:54 +0530

    Update script.js

--- actvity/script.js
+++ actvity/script.js
@@ -1,50 +1,68 @@
-let canvas=document.querySelector("#canvas")
+let canvas = document.querySelector("#canvas");
 
-canvas.height=window.innerHeight-100;
-canvas.width=window.innerWidth;
-window.addEventListener("resize",function(){
-    canvas.height=window.innerHeight-100;
-canvas.width=window.innerWidth;
-})
-let ctx=canvas.getContext("2d");
-//ctx.fillStyle = "green";
-//ctx.fillRect(10, 10, 150, 100);
-let isPendown=false;
-let redolinesDB=[];
-let linesDB=[];
-let line=[];
-canvas.addEventListener("mousedown",function(e){
-    if (redolinesDB.length) {
-        redolinesDB = [];
-      }
-  isPendown=true;  
-  //console.log(e);
-  let x=e.clientX;
-  let y=e.clientY -100;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 100;
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 100;
+  drawLinesFromDB();
+});
+
+// canvas drawing gets erased on window resize ???
+
+// a context object which provides fun for 2d drawing
+let ctx = canvas.getContext("2d");
+
+let linesDB = [];
+let redoLinesDB = [];
+let isPenDown = false;
+let line = [];
+
+canvas.addEventListener("mousedown", function (e) {
+  if (redoLinesDB.length) {
+    redoLinesDB = [];
+  }
+  console.log("Inside mouse down");
+  isPenDown = true;
+  let x = e.clientX;
+  let y = e.clientY - 100;
   ctx.beginPath();
-  ctx.moveTo(x,y);
-  let pointobject={x:x,
-                   y:y,
-                   type:"md" }
-  line.push(pointobject);
-})
-canvas.addEventListener("mousemove",function(e){
-    if (isPendown)
-    {
-        let x=e.clientX;
-        let y=e.clientY-100;
-        ctx.lineTo(x,y);
-        ctx.stroke();
-        let pointobject={x:x,
-            y:y,
-            type:"mm" }
-line.push(pointobject);
-    }
-})
-canvas.addEventListener("mouseup",function(e){
-   isPendown=false;
-   linesDB.push(line);
-  // console.log(linesDB);
-   line=[];
-   
-})
+  ctx.moveTo(x, y);
+
+  let pointObject = {
+    x: x,
+    y: y,
+    type: "md",
+    lineWidth: ctx.lineWidth,
+    strokeStyle: ctx.strokeStyle,
+  };
+  line.push(pointObject);
+});
+
+canvas.addEventListener("mousemove", function (e) {
+  if (isPenDown) {
+    console.log("Inside mousemove");
+    let x = e.clientX;
+    let y = e.clientY - 100;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+    let pointObject = {
+      x: x,
+      y: y,
+      type: "mm",
+    };
+    line.push(pointObject);
+  }
+});
+
+canvas.addEventListener("mouseup", function (e) {
+  console.log("mouseup");
+  isPenDown = false;
+
+  linesDB.push(line);
+  line = [];
+
+  console.log(linesDB);
+});
