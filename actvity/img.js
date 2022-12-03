61bc93eb3b7c118c290a62a36755eb2e3de875d6
adisk01 <81865578+adisk01@users.noreply.github.com>
2022-12-03 13:15:52 +0530

    Update img.js

--- actvity/img.js
+++ actvity/img.js
@@ -1,15 +1,29 @@
-let photodiv=document.querySelector("#photo")
-let photoinput=document.querySelector("#photo-upload");
-photodiv.addEventListener("click",function(){
-    photoinput.click();
-})
-photoinput.addEventListener("change",function(e){
-    console.log(e);
-    let fileObj=e.target.files[0];
-    let filePath=URL.createObjectURL(fileObj,{type:"image/jpg"});
-    console.log(filePath);
-    let img=document.createElement("img");
-    img.setAttribute("src",filePath);
-    img.classList.add("sticky-image");
-    opensticky(img)
+let photoDiv = document.querySelector("#photo");
+let photoUploadInput = document.querySelector("#photo-upload");
+let downloadDiv = document.querySelector("#download");
+
+photoDiv.addEventListener("click", function () {
+  photoUploadInput.click();
+});
+
+photoUploadInput.addEventListener("change", function (event) {
+  console.log(event);
+  let fileObj = event.target.files[0];
+  console.log(fileObj);
+  let filePath = URL.createObjectURL(fileObj, { type: "image/jpg" });
+  let img = document.createElement("img");
+  img.setAttribute("src", filePath);
+  img.classList.add("sticky-image");
+  addSticky(img);
+});
+
+
+downloadDiv.addEventListener("click" , function(){
+    let imagePath = canvas.toDataURL("image/jpg");
+    console.log(imagePath);
+    // <a href="" download="canvas.jpg"></a> 
+    let aTag = document.createElement("a");
+    aTag.download = "canvas.jpg";
+    aTag.href = imagePath;
+    aTag.click();
 })
