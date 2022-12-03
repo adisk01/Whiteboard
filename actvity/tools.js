9eb52f37d997c13b1a6ff8ca04ddcc24b9ede194
adisk01 <81865578+adisk01@users.noreply.github.com>
2022-12-03 13:18:02 +0530

    Update tools.js

--- actvity/tools.js
+++ actvity/tools.js
@@ -1,56 +1,64 @@
-let undo=document.querySelector("#undo");
-let redo=document.querySelector("#redo");
-undo.addEventListener("click",linesundo);
-redo.addEventListener("click",linesredo);
-function linesundo()
-{
-    if(linesDB.length){
-   let undolines= linesDB.pop();
-    redolinesDB.push(undolines);
-    //clear canvas
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    drawlinesfromDB();}
-}
-function drawlinesfromDB()
-{
-    for(let i=0;i<linesDB.length;i++)
-    {
-        let line=linesDB[i];
-        for(let i=0;i<line.length;i++)
-        {
-            let pointobject=line[i];
-            if(pointobject.type=="md")
-            {
-                ctx.beginPath();
-                ctx.moveTo(pointobject.x,pointobject.y);
-            }
-            else
-            {
-                ctx.lineTo(pointobject.x,pointobject.y);
-                ctx.stroke();
-            }
-        }
-    }
+let undo = document.querySelector("#undo");
+let redo = document.querySelector("#redo");
+undo.addEventListener("click", undoLine);
+redo.addEventListener("click", redoLine);
 
+function undoLine() {
+  if (linesDB.length) {
+    let undoLine = linesDB.pop();
+    redoLinesDB.push(undoLine);
+
+    // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawLinesFromDB();
+  }
 }
-function linesredo(){
-    if(redolinesDB.length){
-    let redoline=redolinesDB.pop();
-    for(let i=0;i<redoline.length;i++)
-    {
-        let pointobject=redoline[i];
-        if(pointobject.type=="md")
-        {
-            ctx.beginPath();
-            ctx.moveTo(pointobject.x,pointobject.y);
-        }
-        else
-        {
-            ctx.lineTo(pointobject.x,pointobject.y);
-            ctx.stroke();
-        }
-        linesDB.push(redoline);
-        
+function redoLine() {
+  if (redoLinesDB.length) {
+    
+    let currentLineWidth = ctx.lineWidth;
+    let currentStrokeStyle = ctx.strokeStyle;
+
+    let redoLine = redoLinesDB.pop();
+    for (let i = 0; i < redoLine.length; i++) {
+      let pointObject = redoLine[i];
+      if (pointObject.type == "md") {
+        ctx.lineWidth = pointObject.lineWidth;
+        ctx.strokeStyle = pointObject.strokeStyle;
+        ctx.beginPath();
+        ctx.moveTo(pointObject.x, pointObject.y);
+      } else {
+        ctx.lineTo(pointObject.x, pointObject.y);
+        ctx.stroke();
+      }
     }
+    linesDB.push(redoLine);
+
+    ctx.lineWidth = currentLineWidth;
+    ctx.strokeStyle = currentStrokeStyle;
+  }
 }
+function drawLinesFromDB() {
+  let currentLineWidth = ctx.lineWidth;
+  let currentStrokeStyle = ctx.strokeStyle;
+
+  for (let i = 0; i < linesDB.length; i++) {
+    let line = linesDB[i];
+    for (let i = 0; i < line.length; i++) {
+      let pointObject = line[i];
+      if (pointObject.type == "md") {
+        ctx.lineWidth = pointObject.lineWidth;
+        ctx.strokeStyle = pointObject.strokeStyle;
+        ctx.beginPath();
+        ctx.moveTo(pointObject.x, pointObject.y);
+      } else {
+        ctx.lineTo(pointObject.x, pointObject.y);
+        ctx.stroke();
+      }
+    }
+  }
+
+  ctx.lineWidth = currentLineWidth;
+  ctx.strokeStyle = currentStrokeStyle;
 }
