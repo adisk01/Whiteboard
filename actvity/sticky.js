bb4c5d0959a9f08350f536a8f8904876afad0fcd
adisk01 <81865578+adisk01@users.noreply.github.com>
2022-12-03 13:17:18 +0530

    Update sticky.js

--- actvity/sticky.js
+++ actvity/sticky.js
@@ -1,49 +1,48 @@
-let sticky=document.querySelector("#sticky");
-sticky.addEventListener("click",function(e){
-    opensticky()
-})
-function opensticky(imageElement)
-{
-    let stickydiv=document.createElement("div")
-    stickydiv.classList.add("sticky")
-    stickydiv.innerHTML=`  <div class="sticky-header">
-    <div class="minimize"><i class="fas fa-compress-alt"></i></div>
-    <div class="close"> <i class="fas fa-times"></i></div>
-  </div>`
-  
-  let minimize=stickydiv.querySelector(".minimize");
- // let stickyContent=stickydiv.querySelector(".sticky-content")
-  let close=stickydiv.querySelector(".close");
-  close.addEventListener("click",function(){
-      stickydiv.remove();
-  })
-  let stickyHeader=stickydiv.querySelector(".sticky-header")
+let sticky = document.querySelector("#sticky");
+
+sticky.addEventListener("click", function(e){
+  addSticky();
+});
+
+function addSticky(imageElement) {
+  let stickyDiv = document.createElement("div");
+  stickyDiv.classList.add("sticky");
+  stickyDiv.innerHTML = `<div class="sticky-header">
+    <div class="minimize"></div>
+    <div class="close"></div>
+  </div>`;
+  let minimize = stickyDiv.querySelector(".minimize");
+  let close = stickyDiv.querySelector(".close");
+  let stickyHeader = stickyDiv.querySelector(".sticky-header");
   let stickyContent;
-  if(imageElement)
-  {
-      let stickyImageDiv = document.createElement("div");
-  stickyImageDiv.classList.add("sticky-image-div");
-  stickydiv.append(stickyImageDiv);
-  stickyImageDiv.append(imageElement);
-  stickyContent = stickyImageDiv;
-}
-else{ // <div class="sticky-content" contenteditable="true"></div>
-    let stickyContentDiv = document.createElement("div");
-    stickyContentDiv.classList.add("sticky-content");
-    stickyContentDiv.setAttribute("contenteditable" , "true");
-    stickyContentDiv.setAttribute("spellcheck" , "false");
-    stickydiv.append(stickyContentDiv);
-    stickyContent = stickyContentDiv}
+  if(imageElement){
+    let stickyImageDiv = document.createElement("div");
+    stickyImageDiv.classList.add("sticky-image-div");
+    stickyDiv.append(stickyImageDiv);
+    stickyImageDiv.append(imageElement);
+    stickyContent = stickyImageDiv;
+  }else{
+     // <div class="sticky-content" contenteditable="true"></div>
+     let stickyContentDiv = document.createElement("div");
+     stickyContentDiv.classList.add("sticky-content");
+     stickyContentDiv.setAttribute("contenteditable" , "true");
+     stickyDiv.append(stickyContentDiv);
+     stickyContent = stickyContentDiv;
+  }
+
+  minimize.addEventListener("click", function () {
+    stickyContent.style.display == "none"
+      ? (stickyContent.style.display = "block")
+      : (stickyContent.style.display = "none");
+  });
+
+  close.addEventListener("click", function () {
+    stickyDiv.remove();
+  });
+
   let stickyHold = false;
   let initialX;
   let initialY;
-  minimize.addEventListener("click",function()
-  {
-    stickyContent.style.display == "none"
-    ? (stickyContent.style.display = "block")
-    : (stickyContent.style.display = "none");
-
-  })
   stickyHeader.addEventListener("mousedown", function (e) {
       stickyHold=true;
       initialX = e.clientX;
@@ -58,11 +57,11 @@ else{ // <div class="sticky-content" contenteditable="true"></div>
           let dx = finalX - initialX;
           let dy = finalY - initialY;
     
-          let {top , left} = stickydiv.getBoundingClientRect();
+          let {top , left} = stickyDiv.getBoundingClientRect();
           //   sticky => top + dy
           //  sticky => left + dx
-          stickydiv.style.top = top + dy + "px";
-          stickydiv.style.left = left +dx + "px";
+          stickyDiv.style.top = top + dy + "px";
+          stickyDiv.style.left = left +dx + "px";
     
           initialX = finalX;
           initialY = finalY;
@@ -73,6 +72,5 @@ else{ // <div class="sticky-content" contenteditable="true"></div>
       stickyHold = false;
   });
 
-
-document.body.append(stickydiv);
+  document.body.append(stickyDiv);
 }
